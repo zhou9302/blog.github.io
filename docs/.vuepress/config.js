d16cb5505d3838e8e8624b213ad62c4036ba5795
周颖 <zhouying@rongshantech.com>
2020-09-16 18:04:19 +0800

    feat:vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,29 @@
 module.exports = {
-  title: 'Adroi媒体API 接口文档',  // 设置网站标题
-  description: 'Adroi',
-  base: '/v1/adroi-h5/adroiapi/',
+  title: '大象的blog',  // 设置网站标题
+  description: 'this is a blog',
+  base: '/',
   themeConfig: {
     nav: [
-      { text: '接口定义', link: '/apiword' },
-      { text: '接口字段定义', link: '/api' },
-      { text: '附录：错误码', link: '/error' }
+      { text: 'Home', link: '/home' },
+      { text: 'Guide', link: '/guide' },
+      { text: 'External', link: '/external' }
     ],
     sidebar: {
       '/': [
-        "/", //指的是根目录的md文件 也就是 README.md 里面的内容
-        "apiword", //根目录创建 apiword.md文件
-        "api", //根目录创建 api.md文件
-        "error" //根目录创建 error.md文件
+        {
+          title: 'Group 1',   // 必要的
+          path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1,    // 可选的, 默认值是 1
+          children: [
+            '/'
+          ]
+        },
+        {
+          title: 'Group 2',
+          children: [ /* ... */],
+          initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+        }
       ]
     },
     sidebarDepth: 2
